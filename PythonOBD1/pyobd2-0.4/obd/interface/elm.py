814a148f554d047c9b8650ba7821814afd3d5abf
arvigovi <arvind.govindaraj@gmail.com>
2014-05-12 22:56:25 -0700

    M4

--- PythonOBD1/pyobd2-0.4/obd/interface/elm.py
+++ PythonOBD1/pyobd2-0.4/obd/interface/elm.py
@@ -201,7 +201,7 @@ class ELM32X(Interface):
         # We check the two default baud rates first, then go fastest to
         # slowest, on the theory that anyone who's using a slow baud rate is
         # going to be less picky about the time required to detect it.
-        bauds = [ 38400, 9600, 230400, 115200, 57600, 19200 ]
+        bauds = [ 115200, 115200 ]
 
         for baud in bauds:
             port.set_baudrate(baud)
